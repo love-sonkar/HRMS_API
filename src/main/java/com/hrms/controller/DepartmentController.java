1076e0d066de9be645d314c5eccf555246d67198
AkashPathak1808 <akashpathak180801@gmail.com>
2024-05-03 14:59:36 +0530

    Add controller

--- src/main/java/com/hrms/controller/DepartmentController.java
+++ src/main/java/com/hrms/controller/DepartmentController.java
@@ -1,8 +1,12 @@
 package com.hrms.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +22,25 @@ public class DepartmentController {
 	@Autowired
 	private DepartmentService service;
 
+//	add department
 	@PostMapping("/add")
 	public ResponseEntity<DepartmentDTO> addDepartment(@RequestBody DepartmentDTO departmentDto) {
 		DepartmentDTO createDepartmant = this.service.createDepartmant(departmentDto);
 		return new ResponseEntity<>(createDepartmant, HttpStatus.CREATED);
+	}
+
+//	get all department
+	@GetMapping("/getAll")
+	public ResponseEntity<List<DepartmentDTO>> getAllDepartment() {
+		List<DepartmentDTO> allDepartment = this.service.getAllDepartment();
+		return new ResponseEntity<>(allDepartment, HttpStatus.OK);
+	}
+
+//	get single department
+	@GetMapping("/getDepartment/{id}")
+	public ResponseEntity<DepartmentDTO> getDepartment(@PathVariable Long id) {
+		DepartmentDTO department = this.service.getDepartment(id);
+		return new ResponseEntity<>(department, HttpStatus.OK);
 	}
 
 }
